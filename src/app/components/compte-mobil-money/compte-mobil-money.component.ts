c50f7e1ec6287ac05a567aa846c0cc4991698713
KOUFESSI Roland <koufessirolland@gmail.com>
2024-06-21 00:32:44 +0000

    Add phone number

--- src/app/components/compte-mobil-money/compte-mobil-money.component.ts
+++ src/app/components/compte-mobil-money/compte-mobil-money.component.ts
@@ -3,9 +3,7 @@ import { SideBarComponent } from '../partials/side-bar/side-bar.component';
 import { NavBarComponent } from '../partials/nav-bar/nav-bar.component';
 import { FooterComponent } from '../partials/footer/footer.component';
 import { AsideComponent } from '../partials/aside/aside.component';
-import home from '@iconify/icons-mdi/home';
-import groupAdd from '@iconify/icons-mdi/group-add';
-import bellSlash from '@iconify/icons-fa-solid/bell-slash';
+
 
 @Component({
   selector: 'app-compte-mobil-money',
